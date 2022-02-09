5f544801d1734f46ce054d95f07f71b4ae7f6dc7
Naomi Carrigan <nhcarrigan@gmail.com>
2022-02-08 17:35:50 -0800

    feat: setup infrastructure (#1)
    * feat: set up tooling
    
    * feat: add example algorithm and tests

---
+++ src/_example/exampleAlgorithm.ts
@@ -0,0 +1,8 @@
+/**
+ * This is an example algorithm to demonstrate the syntax and test
+ * runner.
+ *
+ * @param {boolean} bool A boolean.
+ * @returns {boolean} An inverted boolean.
+ */
+export const exampleAlgorithm = (bool: boolean): boolean => !bool;
